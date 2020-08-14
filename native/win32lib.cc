1848d099a9494ba2fab99c7f7032317ba6aa087b
Morris <li081197@gmail.com>
2020-08-14 01:19:48 -0700

    implemented IgnoreShowDesktop

--- native/win32lib.cc
+++ native/win32lib.cc
@@ -11,3 +11,20 @@ BOOL SetBottomWindow(HWND handle) {
         SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE
     );
 }
+
+BOOL IgnoreShowDesktop(HWND handle) {
+    // Might only work on windows 10
+    HWND hWorkerW = NULL;
+    HWND nextWindow = NULL;
+    do {
+        hWorkerW = FindWindowExA(NULL, hWorkerW, "WorkerW", NULL);
+        nextWindow = FindWindowExA(hWorkerW, NULL, "SHELLDLL_DefView", NULL);
+    } while (nextWindow == NULL && hWorkerW != NULL);
+
+    if (nextWindow) {
+        SetWindowLongPtr(handle, -8, (LONG_PTR)nextWindow);
+        return TRUE;
+    }
+    return FALSE;
+
+}
--- native/win32module.cc
+++ native/win32module.cc
@@ -4,7 +4,7 @@
 
 Napi::Boolean SetWindowBottom_Wrapper(Napi::CallbackInfo& info) {
     Napi::Env jsEnv = info.Env();
-    // Check for erroneus arguments from JS call
+    // Check for erroneus JS call
     if (info.Length() > 1) {
         Napi::Error::New(jsEnv, "Too many arguments").ThrowAsJavaScriptException();
         return Napi::Boolean::New(jsEnv, false);
@@ -18,18 +18,34 @@ Napi::Boolean SetWindowBottom_Wrapper(Napi::CallbackInfo& info) {
     HWND* windowHandle = reinterpret_cast<HWND*>(handleData);
 
     BOOL outcome = SetBottomWindow(*windowHandle);
-    if (outcome) {
-        return Napi::Boolean::New(jsEnv, true);
-    } else {
+    return Napi::Boolean::New(jsEnv, outcome);
+}
+
+Napi::Boolean IgnoreShowDesktop_Wrapper(Napi::CallbackInfo& info) {
+    Napi::Env jsEnv = info.Env();
+    // Check for erroneous JS call
+        if (info.Length() > 1) {
+        Napi::Error::New(jsEnv, "Too many arguments").ThrowAsJavaScriptException();
         return Napi::Boolean::New(jsEnv, false);
     }
-
+    if (!info[0].IsBuffer()) {
+        Napi::Error::New(jsEnv, "Argument is not a Buffer").ThrowAsJavaScriptException();
+        return Napi::Boolean::New(jsEnv, false);
+    }
+    char* handleData = info[0].As<Napi::Buffer<char>>().Data();
+    HWND* windowHandle = reinterpret_cast<HWND*>(handleData);
+    BOOL outcome = IgnoreShowDesktop(*windowHandle);
+    return Napi::Boolean::New(jsEnv, outcome);
 }
 
 Napi::Object Init(Napi::Env jsEnv, Napi::Object exports) {
     exports.Set(
         Napi::String::New(jsEnv, "setBottomMost"),
         Napi::Function::New(jsEnv, SetWindowBottom_Wrapper)
+    );
+    exports.Set(
+        Napi::String::New(jsEnv, "ignoreShowDesktop"),
+        Napi::Function::New(jsEnv, IgnoreShowDesktop_Wrapper)
     );
     return exports;
 }
