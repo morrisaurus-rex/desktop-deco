a7caf635b68a01a452c32288d47d8f9d011a7d37
Morris <li081197@gmail.com>
2020-08-15 12:54:13 -0700

    fleshed out settings window

--- ui/ui-scripts/SettingsWindow.js
+++ ui/ui-scripts/SettingsWindow.js
@@ -1,7 +1,8 @@
 const { ipcRenderer } = require('electron');
 
 let button = document.querySelector('#upload-button'),
-    closeButton = document.querySelector('#close-button')
+    closeButton = document.querySelector('#close-button'),
+    exitButton = document.querySelector('#exit-button');
 
 button.addEventListener('click', (event) => {
     ipcRenderer.send('choose-file');
@@ -13,4 +14,8 @@ ipcRenderer.on('ipc-debug', (event, arg) => {
 
 closeButton.addEventListener('click', (event) => {
     ipcRenderer.send('hide-window');
+});
+
+exitButton.addEventListener('click', event => {
+    ipcRenderer.send('quit-application');
 });
