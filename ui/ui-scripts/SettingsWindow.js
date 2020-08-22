2906c58be911b770da83811c1b9b11ea2a65d730
Morris <li081197@gmail.com>
2020-08-22 00:18:06 -0700

    implement layout loading

--- ui/ui-scripts/SettingsWindow.js
+++ ui/ui-scripts/SettingsWindow.js
@@ -1,8 +1,51 @@
 const { ipcRenderer } = require('electron');
+let app = null, path = null, fs = null;
 
+// Button selectors
 let button = document.querySelector('#upload-button'),
     closeButton = document.querySelector('#close-button'),
-    exitButton = document.querySelector('#exit-button');
+    exitButton = document.querySelector('#exit-button'),
+    testButton = document.querySelector('#test-button');
+
+// Content box slectors
+let layoutList = document.querySelector('#layout-list');
+
+
+function getLayoutInfo(callback) {
+    if (app == null) {
+        app = require('electron').app;
+        path = require('path');
+        fs = require('fs');
+    }
+    let appDataPath = app.getPath("userData");
+    fs.readFile(path.join(appDataPath, 'config'), (data, err) => {
+        // TODO: Consider caching user configs to reduce frequency of file reads
+        let configObj = JSON.parse(data);
+        callback(configObj);
+    });
+}
+
+function populateLayoutList(config) {
+    while (layoutList.hasChildNodes()) {
+        layoutList.removeChild(layoutList.firstChild);
+    }
+    let layouts = config["savedLayouts"];
+    if (layouts.length == 0) {
+        let noLayouts = document.createElement('p');
+        noLayouts.innerHTML = 'No layouts found';
+        layoutList.appendChild(noLayouts)
+    }
+}
+
+function createLayoutListItem(layoutName) {
+    let item = document.createElement('div');
+    item.setAttribute('class', 'layout-list-item');
+    
+}
+
+function layoutListClick(layoutName ,event) {
+
+}
 
 button.addEventListener('click', (event) => {
     ipcRenderer.send('choose-file');
@@ -18,4 +61,9 @@ closeButton.addEventListener('click', (event) => {
 
 exitButton.addEventListener('click', event => {
     ipcRenderer.send('quit-application');
+});
+
+testButton.addEventListener('click', event => {
+    // Used for testing some kind of behaviour or functionality
+    ipcRenderer.send('test-function');
 });
