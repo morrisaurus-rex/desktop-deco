44eed199300bb3c77df1e54c6287b0d913ee7b56
Morris <li081197@gmail.com>
2020-08-22 00:17:38 -0700

    Added comments

--- WidgetManager.js
+++ WidgetManager.js
@@ -1,5 +1,7 @@
 const { BrowserWindow } = require("electron");
 const isWin32 = require('os').platform == 'win32';
+const nativeaddon = (isWin32) ? require('bindings')('win32lib'): null;
+const path = require('path');
 
 const nonAdjustableProps = {
     frame: false,
@@ -27,19 +29,22 @@ class WidgetManager {
         this.widgets = [];
         this.interactable = false;
     }
-
+    // Adds a widget to be displayed
     addWidget(widgetName, filename) {
+        if (path.extname(filename) != '.html') {
+            throw new TypeError('Invalid file extension');
+        }
         let window = (this.interactable) ? new BrowserWindow(adJustableProps) : new BrowserWindow(nonAdjustableProps);
         window.loadFile(filename);
         window.show();
         this.widgets.push({
-            "filename": filename,
+            "filename": filename, 
             "name": widgetName,
             "browserWin": window
         });
-        return;
+        return this.widgets[this.widgets.length - 1];
     }
-
+    // Redraws currently managed widgets with a framed window
     toggleAdjustable() {
         for (let a = 0; a < this.widgets.length; a++) {
             let windowDims = this.widgets[a]["browserWin"].getSize();
@@ -50,10 +55,15 @@ class WidgetManager {
             replacementWindow.loadFile(this.widgets[a]["browserWin"].filename);
             this.widgets[a]["browserWin"].destroy();
             replacementWindow.show()
+            if (this.interactable && isWin32) {
+                let hwnd = replacementWindow.getNativeWindowHandle();
+                nativeaddon.setBottomMost(hwnd);
+                nativeaddon.ignoreShowDesktop(hwnd);
+            }
             this.widgets[a]["browserWin"] = replacementWindow;
         }
     }
-
+    // Creates a configuration array, allowing the app to store desktop state in a JSON file
     getConfig() {
         let configs = [];
         for (let a = 0; a < this.widgets.length; a++) {
@@ -71,7 +81,23 @@ class WidgetManager {
         return configs;
     }
 
-    parseConfig(filepath) {
+    parseConfig(configArray) {
+        // Clear existing widget array
+        while (this.widgets.length != 0) {
+            let tempWidget = this.widgets.pop();
+            tempWidget["browserWin"].destroy();
+        }
+
+        for (let index in configArray) {
+            this.interactable = true;
+            let addedWidget = this.addWidget(configArray[index].name, configArray[index].filename);
+            addedWidget.browserWin.setPosition(configArray[index].x, configArray[index].y);
+            addedWidget.browserWin.setSize(configArray[index].width, configArray[index].height);
+        }
+
+        return;
 
     }
 }
+
+exports.WidgetManager = WidgetManager;
